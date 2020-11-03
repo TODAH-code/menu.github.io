24d66143396f6aea782b9398066309ab3875e283
TODAH-code <victorgmanco@gmail.com>
2020-11-02 23:01:31 -0300

    efects

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-function closeFilter() {
-    document.getElementById('mobile-filter').style.width = '0%'
-}
-function openFilter() {
-    document.getElementById('mobile-filter').style.width = '100%'
+const filter_events = {
+    closeFilter: () => {
+        document.getElementById('mobile-filter').style.width = '0%'
+    },
+    openFilter: () => {
+        document.getElementById('mobile-filter').style.width = '100%'
+    }
 }
 
 function how(event) {
     var el = event.target
     var ul = el.nextElementSibling
-    var display = ul.style.display == 'none' ? 'block' : 'none'
-
-    ul.style.display = display
-
+    if (el.nodeName == 'H4' && el.className == 'filter__title') {
+        var display = ul.style.display == 'block' ? 'none' : 'block'
+        console.log(el.nodeName);
+        ul.style.display = display
+    }
 }
 document.body.addEventListener('click', how)
